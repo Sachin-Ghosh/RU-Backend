e0e2785a0b39616a289918c478262b2c3c584e64
Sachin Ghosh <ghoshsachin2301@gmail.com>
2025-01-29 20:24:22 +0530

    v0.4

--- build.sh
+++ build.sh
@@ -2,18 +2,24 @@
 # exit on error
 set -o errexit
 
+# Create a virtual environment
+python -m venv .venv
+source .venv/bin/activate
+
 echo "Installing system dependencies..."
-apt-get update && apt-get install -y \
+apt-get update
+apt-get install -y \
     python3-pip python3-dev \
     postgresql postgresql-contrib \
     libpq-dev gcc \
-    opencv-python-headless
+    tesseract-ocr \
+    libgl1-mesa-glx
 
 echo "Upgrading pip..."
 python -m pip install --upgrade pip
 
 echo "Installing Python dependencies..."
-pip install -r requirements.txt
+pip install --no-cache-dir -r requirements.txt
 
 echo "Installing gunicorn..."
 pip install gunicorn
