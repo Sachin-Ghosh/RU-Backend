cad5c86b96f31382ab66d8acb1bf37af16197369
Sachin Ghosh <ghoshsachin2301@gmail.com>
2025-04-30 23:02:20 +0530

    made changes

--- authentication/models.py
+++ authentication/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from accounts.models import User
 
-# Create your models here.
 
 class AuthenticationLog(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='auth_logs')
